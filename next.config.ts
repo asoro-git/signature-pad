95d79dab66bb8a1e32847dbe6b51b5c665d9a736
study s <ohvelynuna@gmail.com>
2025-05-02 19:58:00 +1000

    redirect traffic

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/signPad",
+  trailingSlash: true,
 };
 
 export default nextConfig;
