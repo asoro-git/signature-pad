592e76fbf2df82be44a96d05d0582de6995774e8
study s <ohvelynuna@gmail.com>
2025-05-03 19:59:37 +1000

    redirect root

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  basePath: "/signpad",
+    /* config options here */
+    basePath: "/signpad",
+    async redirects() {
+        return [
+            {
+                // does not add /docs since basePath: false is set
+                source: "/",
+                destination: "http://sydneyot.com/signpad",
+                basePath: false,
+                permanent: true,
+            },
+        ];
+    },
 };
 
 export default nextConfig;
